6d22266c214e96627c1fd56e4a06df50096b6480
Juklok <107051800+JoshuaKlot@users.noreply.github.com>
2024-11-13 18:28:02 -0500

    Add files via upload

--- dualrow.c
+++ dualrow.c
@@ -40,7 +40,7 @@ int find_min_cost(Block *blocks, int num_blocks, int index, Row row1, Row row2,
     }
 
     // Memoization check: return if this state is already computed
-    if (dp[index][row1_length][row2_length] != INT_MAX) return dp[index][row1_length][row2_length];
+    if (dp[index][row1_length][row2_length] <= current_cost) return dp[index][row1_length][row2_length];
 
     Block block = blocks[index];
     int cost_with_row1 = INT_MAX, cost_with_row2 = INT_MAX;
@@ -50,6 +50,11 @@ int find_min_cost(Block *blocks, int num_blocks, int index, Row row1, Row row2,
         int cost_row1 = pow(block.x - row1.x, 2) + pow(block.y - row1.y, 2);
         row1_blocks[row1_count] = index;
         Row new_row1 = {row1.x + block.length, row1.y};
+        //printf("Putting Block %d in row 0 for an added cost of %d to the current cost of %d total cost of %d\n",index,cost_row1,current_cost,current_cost+cost_row1);
+        if(index==num_blocks-1)
+        {
+            //printf("This is the last block placed on this side of the tree making the final cost %d\n",current_cost+cost_row1);
+        }
         cost_with_row1 = find_min_cost(blocks, num_blocks, index + 1, new_row1, row2,
                                        row1_length + block.length, row2_length, target_length,
                                        current_cost + cost_row1, row1_blocks, row1_count + 1,
@@ -61,6 +66,11 @@ int find_min_cost(Block *blocks, int num_blocks, int index, Row row1, Row row2,
         int cost_row2 = pow(block.x - row2.x, 2) + pow(block.y - row2.y, 2);
         row2_blocks[row2_count] = index;
         Row new_row2 = {row2.x + block.length, row2.y};
+        //printf("Putting Block %d in row 0 for an added cost of %d to the current cost of %d total cost of %d\n",index,cost_row2,current_cost,current_cost+cost_row2);
+        if(index==num_blocks-1)
+        {
+            //printf("This is the last block placed on this side of the tree making the final cost %d\n",current_cost+cost_row2);
+        }
         cost_with_row2 = find_min_cost(blocks, num_blocks, index + 1, row1, new_row2,
                                        row1_length, row2_length + block.length, target_length,
                                        current_cost + cost_row2, row1_blocks, row1_count,
@@ -73,39 +83,58 @@ int find_min_cost(Block *blocks, int num_blocks, int index, Row row1, Row row2,
 }
 
 void print_table(Result *result, Block *blocks, int num_blocks) {
-    printf("Blk     ");
-    for (int i = 0; i < num_blocks; i++) {
-        printf("%10d ", i);
-    }
-    printf("Total Length\n");
 
-    int row1_length = 0, row2_length = 0;
-    printf("Row 1 :");
-    for (int i = 0; i < num_blocks; i++) {
-        if (i < result->best_row1_count) {
-            int block_idx = result->best_row1_blocks[i];
-            Block block = blocks[block_idx];
-            row1_length += block.length;
-            printf("[$%3d \\%3d] ", block.x, block.y);
-        } else {
-            printf("[         ] ");
-        }
+    // Display all blocks and where they were moved
+    for (int i = 0; i < result->best_row1_count; i++) {
+        int block_idx = result->best_row1_blocks[i];
+        Block block = blocks[block_idx];
+        printf("Block %d (length %d) on row 1\n", block_idx + 1, block.length);
     }
-    printf("  %d\n", row1_length);
 
-    printf("Row 2 :");
-    for (int i = 0; i < num_blocks; i++) {
-        if (i < result->best_row2_count) {
-            int block_idx = result->best_row2_blocks[i];
-            Block block = blocks[block_idx];
-            row2_length += block.length;
-            printf("[$%3d \\%3d] ", block.x, block.y);
-        } else {
-            printf("[         ] ");
-        }
+    for (int i = 0; i < result->best_row2_count; i++) {
+        int block_idx = result->best_row2_blocks[i];
+        Block block = blocks[block_idx];
+        printf("Block %d (length %d) on row 2\n", block_idx + 1, block.length);
     }
-    printf("  %d\n", row2_length);
+
+    printf("\n");
+
+    // Now, print the details for each row
+    printf("Row 1 placements:\n");
+    int row1_length = 0;
+    for (int i = 0; i < result->best_row1_count; i++) {
+        int block_idx = result->best_row1_blocks[i];
+        Block block = blocks[block_idx];
+        
+        int cost = pow(block.x - row1.x, 2) + pow(block.y - row1.y, 2);
+        printf("Block %d ( %3d, %3d) -> ( %3d, %3d) cost %d\n", block_idx + 1, row1.x, row1.y, row1.x + block.length, row1.y);
+        
+        // Update row position after placing the block
+        row1.x += block.length;
+        row1_length += block.length;
+    }
+
+    printf("Row 1 total length: %d\n", row1_length);
+
+    printf("\nRow 2 placements:\n");
+    int row2_length = 0;
+    for (int i = 0; i < result->best_row2_count; i++) {
+        int block_idx = result->best_row2_blocks[i];
+        Block block = blocks[block_idx];
+
+        int cost = pow(block.x - row2.x, 2) + pow(block.y - row2.y, 2);
+        printf("Block %d ( %3d, %3d) -> ( %3d, %3d) cost %d\n", block_idx + 1, row2.x, row2.y, row2.x + block.length, row2.y);
+        
+        // Update row position after placing the block
+        row2.x += block.length;
+        row2_length += block.length;
+    }
+
+    printf("Row 2 total length: %d\n", row2_length);
+    printf("Total cost: %d\n", result->min_cost);
 }
+
+
 
 int main() {
     Row row1, row2;
